2aae84bffc35334001d006a8d1cd17c828843899
shankha.sinha <shankha.sinha@planview.com>
2024-04-12 12:23:33 +0530

    longest common prefix

---
+++ LeetCode/Python/longest_common_prefix.py
@@ -0,0 +1,32 @@
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        if len(strs) == 1:
+            return strs[0]
+
+        strs.sort(key=len)
+
+        res = ""
+
+        for i in range(len(strs[0])):
+
+            for s in strs:
+
+                if s[i] != strs[0][i]:
+                    
+                    return res
+            
+            res += strs[0][i]
+
+        return res
+    
+
+
+if __name__ == "__main__":
+    
+    strs1 = ["flower","flow","flight"]
+    
+    s  = Solution()
+    
+    print(f" LCP op {strs1} is {s.longestCommonPrefix(strs1)}")
